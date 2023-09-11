8518e04fa5a23ddf1b2fcbd7d72eaa43ebefdcb9
gaboolic <23441099@qq.com>
2023-09-11 18:29:14 +0800

    添加'兵'行动路线

--- src/main/java/tk/gbl/chessmodel/Pawn.java
+++ src/main/java/tk/gbl/chessmodel/Pawn.java
@@ -4,11 +4,12 @@ import tk.gbl.constant.GameConstant;
 import tk.gbl.model.Chessboard;
 import tk.gbl.model.Point;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 卒
- *
+ * <p>
  * Date: 2017/11/15
  * Time: 15:36
  *
@@ -17,7 +18,7 @@ import java.util.List;
 public class Pawn extends Chessman {
     @Override
     public String getChineseName() {
-        if(getColor() == GameConstant.red) {
+        if (getColor() == GameConstant.red) {
             return "兵";
         }
         return "卒";
@@ -25,7 +26,46 @@ public class Pawn extends Chessman {
 
     @Override
     public List<Point> getMovePoints(Chessboard chessboard) {
-        return null;
+        List<Point> movePoints = new ArrayList<>();
+        int startX = getPoint().getX();
+        int startY = getPoint().getY();
+        int forwardDirection = getColor() == GameConstant.red ? 1 : -1; // 兵的前进方向
+
+        // 判断兵是否可以前进一步
+        int targetX = startX + forwardDirection;
+        int targetY = startY;
+        if (isValidMove(targetX, targetY, chessboard)) {
+            movePoints.add(new Point(targetX, targetY));
+        }
+
+        // 判断兵是否过河
+        boolean isCrossedRiver = getColor() == GameConstant.red ? startX >= 5 : startX <= 4;
+
+        // 如果兵过河了，则判断是否可以左右移动
+        if (isCrossedRiver) {
+            if (isValidMove(startX, startY - 1, chessboard)) {
+                movePoints.add(new Point(targetX, targetY));
+            }
+            if (isValidMove(startX, startY + 1, chessboard)) {
+                movePoints.add(new Point(targetX, targetY));
+            }
+        }
+
+        return movePoints;
     }
 
+    private boolean isValidMove(int x, int y, Chessboard chessboard) {
+        // 判断目标位置是否在棋盘范围内
+        if (!chessboard.isInsideBoard(x, y)) {
+            return false;
+        }
+
+        // 判断目标位置是否为空或者有敌方棋子
+        Chessman targetChessman = chessboard.getChessman(x, y);
+        if (targetChessman == null || targetChessman.getColor() != getColor()) {
+            return true;
+        }
+
+        return false;
+    }
 }
