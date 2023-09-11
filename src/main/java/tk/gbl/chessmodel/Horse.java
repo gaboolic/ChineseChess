56be25e397fc62d2ffab8a7746e28eb017c06596
gaboolic <23441099@qq.com>
2023-09-11 18:25:11 +0800

    添加'马' '帅'行动路线

--- src/main/java/tk/gbl/chessmodel/Horse.java
+++ src/main/java/tk/gbl/chessmodel/Horse.java
@@ -3,6 +3,7 @@ package tk.gbl.chessmodel;
 import tk.gbl.model.Chessboard;
 import tk.gbl.model.Point;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,7 +22,44 @@ public class Horse extends Chessman {
 
     @Override
     public List<Point> getMovePoints(Chessboard chessboard) {
-        return null;
+        List<Point> movePoints = new ArrayList<>();
+        int startX = getPoint().getX();
+        int startY = getPoint().getY();
+        int[][] knightMoves = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}}; // 马的八个跳跃方向
+
+        for (int[] knightMove : knightMoves) {
+            int dx = knightMove[0];
+            int dy = knightMove[1];
+            int targetX = startX + dx;
+            int targetY = startY + dy;
+
+            if (isValidMove(targetX, targetY, chessboard)) {
+                movePoints.add(new Point(targetX, targetY));
+            }
+        }
+
+        return movePoints;
+    }
+
+    private boolean isValidMove(int x, int y, Chessboard chessboard) {
+        // 判断目标位置是否在棋盘范围内
+        if (!chessboard.isInsideBoard(x, y)) {
+            return false;
+        }
+
+        // 判断目标位置是否为空或者有敌方棋子
+        Chessman targetChessman = chessboard.getChessman(x, y);
+        if (targetChessman == null || targetChessman.getColor() != getColor()) {
+            // 判断是否有拌马腿的情况
+            int blockX = x - (x - getPoint().getX()) / 2; // 马腿所在位置的x坐标
+            int blockY = y - (y - getPoint().getY()) / 2; // 马腿所在位置的y坐标
+
+            if (chessboard.getChessman(blockX, blockY) == null) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
--- src/main/java/tk/gbl/chessmodel/King.java
+++ src/main/java/tk/gbl/chessmodel/King.java
@@ -4,11 +4,12 @@ import tk.gbl.constant.GameConstant;
 import tk.gbl.model.Chessboard;
 import tk.gbl.model.Point;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 将帅
- *
+ * <p>
  * Date: 2017/11/15
  * Time: 15:32
  *
@@ -17,7 +18,7 @@ import java.util.List;
 public class King extends Chessman {
     @Override
     public String getChineseName() {
-        if(getColor() == GameConstant.red) {
+        if (getColor() == GameConstant.red) {
             return "帅";
         }
         return "将";
@@ -25,7 +26,46 @@ public class King extends Chessman {
 
     @Override
     public List<Point> getMovePoints(Chessboard chessboard) {
-        return null;
+        List<Point> movePoints = new ArrayList<>();
+        int startX = getPoint().getX();
+        int startY = getPoint().getY();
+        int[][] kingMoves = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 帅的四个直线方向
+
+        for (int[] kingMove : kingMoves) {
+            int dx = kingMove[0];
+            int dy = kingMove[1];
+            int targetX = startX + dx;
+            int targetY = startY + dy;
+
+            if (isValidMove(targetX, targetY, chessboard)) {
+                movePoints.add(new Point(targetX, targetY));
+            }
+        }
+
+        return movePoints;
+    }
+
+    private boolean isValidMove(int x, int y, Chessboard chessboard) {
+        // 判断目标位置是否在九宫格内
+        if (!isInPalace(x, y)) {
+            return false;
+        }
+
+        // 判断目标位置是否为空或者有敌方棋子
+        Chessman targetChessman = chessboard.getChessman(x, y);
+        if (targetChessman == null || targetChessman.getColor() != getColor()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isInPalace(int x, int y) {
+        if (getColor() == GameConstant.red) {
+            return x >= 3 && x <= 5 && y >= 7 && y <= 9;
+        } else {
+            return x >= 3 && x <= 5 && y >= 0 && y <= 2;
+        }
     }
 
 }
